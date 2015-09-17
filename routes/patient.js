65ebfb5e5b4bb49741e8f6f836b3c98fdbd7d60d
mangochiman <mangochiman@gmail.com>
2015-09-17 14:21:28 +0200

    Working on patient confirmation page

--- routes/patient.js
+++ routes/patient.js
@@ -15,59 +15,41 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/scan_barcode', /*loadUser,*/ function (req, res, next) {
-    console.log('Not found = ' + req.session.patient_not_found);
     //patient_not_found = req.session.patient_not_found;
     patient_not_found = req.session.patient_not_found;
+    req.session.person = {};
     req.session.patient_not_found = null;
     res.render('scan_barcode', {title: 'Scan Barcode', patient_not_found: patient_not_found});
 });
 
 router.get('/show/:identifier?', /*loadUser,*/ function (req, res, next) {
-    patientIdentifier = req.query.identifier;
 
-    data = {person: {value: patientIdentifier}};
-    //console.log(data);
-    var args = {
-        data: data,
-        headers: {"Content-Type": "application/json"}
-    };
-
-    client.post(bartAddress, args, function (data, resp) {
-        var person = JSON.parse(data);
-        if (isEmpty(person) === true) {
-            req.session.patient_not_found = 'true'
-            res.redirect("/patients/scan_barcode");
-        }
-
-        else {
-            personAddress = person["person"]["addresses"];
-            personAttributes = person["person"]["attributes"];
-            personNames = person["person"]["names"];
-            patientIdentifiers = person["person"]["patient"]["identifiers"];
-            gender = person["person"]["gender"];
-            birthDay = person["person"]["birth_day"];
-            birthMonth = person["person"]["birth_month"];
-            birthYear = person["person"]["birth_year"];
-            if (birthDay === 'Unknown')
-                birthDay = 1;
-            if (birthMonth === 'Unknown')
-                birthMonth = 7;
-            birthdate = new Date(parseInt(birthYear), parseInt(birthMonth) - 1, parseInt(birthDay));
-            age = getAge(birthdate);
-
-            res.render('show', {title: 'Patients Home Page', personAddress: personAddress,
-                personAttributes: personAttributes, personNames: personNames,
-                patientIdentifiers: patientIdentifiers, gender: gender, birthDay: birthDay,
-                birthMonth: birthMonth, birthYear: birthYear, age: age
-            });
-        }
-    }).on('error', function (err) {
-        console.log('Error')
+    var person = req.session.person;
+    if (isEmpty(person) === true) {
+        req.session.patient_not_found = 'true'
         res.redirect("/patients/scan_barcode");
-        //URL not found
-        //response.redirect('/users/login');
-    });
+    }
 
+    personAddress = person["person"]["addresses"];
+    personAttributes = person["person"]["attributes"];
+    personNames = person["person"]["names"];
+    patientIdentifiers = person["person"]["patient"]["identifiers"];
+    gender = person["person"]["gender"];
+    birthDay = person["person"]["birth_day"];
+    birthMonth = person["person"]["birth_month"];
+    birthYear = person["person"]["birth_year"];
+    if (birthDay === 'Unknown')
+        birthDay = 1;
+    if (birthMonth === 'Unknown')
+        birthMonth = 7;
+    birthdate = new Date(parseInt(birthYear), parseInt(birthMonth) - 1, parseInt(birthDay));
+    age = getAge(birthdate);
+
+    res.render('show', {title: 'Patients Home Page', personAddress: personAddress,
+        personAttributes: personAttributes, personNames: personNames,
+        patientIdentifiers: patientIdentifiers, gender: gender, birthDay: birthDay,
+        birthMonth: birthMonth, birthYear: birthYear, age: age
+    });
 });
 
 router.get('/confirm/:identifier?', /*loadUser,*/ function (req, res, next) {
@@ -98,9 +80,10 @@ router.get('/confirm/:identifier?', /*loadUser,*/ function (req, res, next) {
                 birthDay = 1;
             if (birthMonth === 'Unknown')
                 birthMonth = 7;
-             console.log(patientIdentifiers)
+
             birthdate = new Date(parseInt(birthYear), parseInt(birthMonth) - 1, parseInt(birthDay));
             age = getAge(birthdate);
+            req.session.person = person;
 
             res.render('confirm', {title: 'Confirmation Page', personAddress: personAddress,
                 personAttributes: personAttributes, personNames: personNames,
