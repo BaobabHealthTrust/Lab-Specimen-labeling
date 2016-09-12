7b3a0dc7d7b90828660a9d6f49aa51842fc1aa21
mangochiman <mangochiman@gmail.com>
2016-09-12 14:41:28 +0200

    Removed the # in the print string that was causing other text not to appear in the print out

--- routes/patient.js
+++ routes/patient.js
@@ -328,11 +328,11 @@ router.get('/download_order/:identifier?', loadUser, function (req, res, next) {
                 "q500\n" +
                 "Q165,026\n" +
                 "ZT\n" +
-                "B50,107,0,1,4,8,50,N,\"" + accessionNum + "\"\n" +
-                "A35,30,0,2,1,1,N,\"" + name + "\"\n" +
-                "A35,50,0,2,1,1,N,\"Acc #: " + accessionNum + "\"\n" +
-                "A35,70,0,2,1,1,N,\"Order: " + testName + "\"\n" +
-                "A35,90,0,2,1,1,N,\"" + dateTimeOrdered + "\"\n" +
+                "B50,119,0,1,4,8,50,N,\"" + accessionNum + "\"\n" +
+                "A35,50,0,2,1,1,N,\"" + name + "\"\n" +
+                "A35,70,0,2,1,1,N,\"Acc no  " + accessionNum + "\"\n" +
+                "A35,90,0,2,1,1,N,\"Order " + testName + "\"\n" +
+                "A35,110,0,2,1,1,N,\"" + dateTimeOrdered + "\"\n" +
                 "P3"
 
         fs.writeFile(fileName, data, function (err) {
