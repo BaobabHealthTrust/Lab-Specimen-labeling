fc079bc65d70f9617a67e37b3260d8b128e4c3f2
mangochiman <mangochiman@gmail.com>
2016-11-07 11:52:04 +0200

    Changing printout coordinates

--- routes/patient.js
+++ routes/patient.js
@@ -351,9 +351,10 @@ router.get('/download_order/:identifier?', loadUser, function (req, res, next) {
         fname = new Date().getTime() + '.lbl'
         fileName = '/tmp/' + fname;
         var data = "\nN\n" +
-                "R216,0\n" +
+                "q500\n" +
+                "Q165,026\n" +
                 "ZT\n" +
-                "B50,110,0,1,4,8,50,N,\"" + accessionNum + "\"\n" +
+                "B50,110,0,1,4,4,30,N,\"" + accessionNum + "\"\n" +
                 "A35,50,0,2,1,1,N,\"" + name + "\"\n" +
                 "A35,70,0,2,1,1,N,\"Acc no  " + accessionNum + "\"\n" +
                 "A35,90,0,2,1,1,N,\"Order " + testName + "\"\n" +
